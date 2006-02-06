b39f59756d0437e8870ed059ff97e8b164bdad51
Knut Urdalen <knut.urdalen@gmail.com>
2006-02-06 09:14:25 +0000

    added a lint task for checking the syntax of one or more php source files using the -l flag on the php-cli assuming 'php' is in path

---
+++ classes/phing/tasks/ext/PhpLintTask.php
@@ -0,0 +1,76 @@
+<?php
+require_once 'phing/Task.php';
+
+/**
+ * A PHP lint task. Checking syntax of one or more PHP source file.
+ *
+ * @author   Knut Urdalen <knut.urdalen@telio.no>
+ * @package  phing.tasks.ext
+ */
+class PhpLintTask extends Task {
+
+  protected $file;  // the source file (from xml attribute)
+  protected $filesets = array(); // all fileset objects assigned to this task
+
+  /**
+   * File to be performed syntax check on
+   * @param PhingFile $file
+   */
+  public function setFile(PhingFile $file) {
+    $this->file = $file;
+  }
+  
+  /**
+   * Nested creator, creates a FileSet for this task
+   *
+   * @return FileSet The created fileset object
+   */
+  function createFileSet() {
+    $num = array_push($this->filesets, new FileSet());
+    return $this->filesets[$num-1];
+  }
+
+  /**
+   * Execute lint check against PhingFile or a FileSet
+   */
+  public function main() {
+    if($this->file instanceof PhingFile) {
+      $this->lint($this->file->getPath());
+    } else { // process filesets
+      $project = $this->getProject();
+      foreach($this->filesets as $fs) {
+	$ds = $fs->getDirectoryScanner($project);
+	$files = $ds->getIncludedFiles();
+	foreach($files as $file) {
+	  $this->lint($file);
+	}
+      }
+    }
+    $this->log('No syntax errors detected');
+  }
+
+  /**
+   * Performs the actual syntax check
+   *
+   * @param string $file
+   * @return void
+   */
+  protected function lint($file) {
+    $command = 'php -l ';
+    if(file_exists($file)) {
+      if(is_readable($file)) {
+	$message = array();
+	exec($command.$file, $message);
+	if(!preg_match('/^No syntax errors detected/', $message[0])) {
+	  throw new BuildException($message[1]);
+	}
+      } else {
+	throw new BuildException('Permission denied: '.$file);
+      }
+    } else {
+      throw new BuildException('File not found: '.$file);
+    }
+  }
+}
+
+?>
