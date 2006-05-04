f1c3a97c45063ce0e6046f01f74aa459b5e475dd
Michiel Rook <mrook@php.net>
2006-05-04 12:12:43 +0000

    Change indentation

--- classes/phing/tasks/ext/phpdoc/PHPDocumentorTask.php
+++ classes/phing/tasks/ext/phpdoc/PHPDocumentorTask.php
@@ -1,164 +1,164 @@
 <?php
 
+/**
+ * $Id$
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information please see
+ * <http://phing.info>.
+ */
+
+require_once 'phing/Task.php';
+
+/**
+ * Task to run phpDocumentor.
+ *
+ * @author Michiel Rook <michiel@trendserver.nl>
+ * @version $Id$
+ * @package phing.tasks.ext.phpdoc
+ */	
+class PHPDocumentorTask extends Task
+{
 	/**
-	 * $Id$
-	 *
-	 * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-	 * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-	 * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-	 * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-	 * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-	 * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-	 * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-	 * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-	 * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-	 * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-	 * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-	 *
-	 * This software consists of voluntary contributions made by many individuals
-	 * and is licensed under the LGPL. For more information please see
-	 * <http://phing.info>.
+	 * The name of the executable for phpDocumentor
 	 */
-	 
-	require_once 'phing/Task.php';
+	const PHPDOC = 'phpdoc';
+
+	private $title = "Default Title";
+
+	private $destdir = ".";
+
+	private $sourcepath = NULL;
+
+	private $output = "";
+
+	private $linksource = false;
+
+	private $parseprivate = false;
 
 	/**
-	 * Task to run phpDocumentor.
-	 *
-	 * @author Michiel Rook <michiel@trendserver.nl>
-	 * @version $Id$
-	 * @package phing.tasks.ext.phpdoc
-	 */	
-	class PHPDocumentorTask extends Task
+	 * Set the title for the generated documentation
+	 */
+	function setTitle($title)
 	{
-		/**
-		 * The name of the executable for phpDocumentor
-		 */
-		const PHPDOC = 'phpdoc';
-		
-		private $title = "Default Title";
-				
-		private $destdir = ".";
-				
-		private $sourcepath = NULL;
-				
-		private $output = "";
-				
-		private $linksource = false;
-		
-		private $parseprivate = false;
+		$this->title = $title;
+	}
 
-		/**
-		 * Set the title for the generated documentation
-		 */
-		function setTitle($title)
+	/**
+	 * Set the destination directory for the generated documentation
+	 */
+	function setDestdir($destdir)
+	{
+		$this->destdir = $destdir;
+	}
+
+	/**
+	 * Set the source path
+	 */
+	function setSourcepath(Path $sourcepath)
+	{
+		if ($this->sourcepath === NULL)
 		{
-			$this->title = $title;
+			$this->sourcepath = $sourcepath;
 		}
-		
-		/**
-		 * Set the destination directory for the generated documentation
-		 */
-		function setDestdir($destdir)
+		else
 		{
-			$this->destdir = $destdir;
+			$this->sourcepath->append($sourcepath);
 		}
-		
-		/**
-		 * Set the source path
-		 */
-		function setSourcepath(Path $sourcepath)
+	}
+
+	/**
+	 * Set the output type
+	 */		
+	function setOutput($output)
+	{
+		$this->output = $output;
+	}
+
+	/**
+	 * Should sources be linked in the generated documentation
+	 */
+	function setLinksource($linksource)
+	{
+		$this->linksource = $linksource;
+	}
+
+	/**
+	 * Should private members/classes be documented
+	 */
+	function setParseprivate($parseprivate)
+	{
+		$this->parseprivate = $parseprivate;
+	}
+
+	/**
+	 * Main entrypoint of the task
+	 */
+	function main()
+	{
+		$arguments = $this->constructArguments();
+
+		$this->log("Running phpDocumentor...");
+
+		exec(self::PHPDOC . " " . $arguments, $output, $return);
+
+		if ($return != 0)
 		{
-			if ($this->sourcepath === NULL)
-			{
-				$this->sourcepath = $sourcepath;
-			}
-			else
-			{
-				$this->sourcepath->append($sourcepath);
-			}
+			throw new BuildException("Could not execute ionCube Encoder: " . implode(' ', $output));
 		}
-		
-		/**
-		 * Set the output type
-		 */		
-		function setOutput($output)
+	}
+
+	/**
+	 * Constructs an argument string for phpDocumentor
+	 */
+	private function constructArguments()
+	{
+		$arguments = "-q on ";
+
+		if ($this->title)
 		{
-			$this->output = $output;
+			$arguments.= "-ti \"" . $this->title . "\" ";
 		}
-		
-		/**
-		 * Should sources be linked in the generated documentation
-		 */
-		function setLinksource($linksource)
+
+		if ($this->destdir)
 		{
-			$this->linksource = $linksource;
+			$arguments.= "-t " . $this->destdir . " ";
 		}
-		
-		/**
-		 * Should private members/classes be documented
-		 */
-		function setParseprivate($parseprivate)
+
+		if ($this->sourcepath !== NULL)
 		{
-			$this->parseprivate = $parseprivate;
+			$arguments.= "-d " . $this->sourcepath->__toString() . " ";
 		}
-		
-		/**
-		 * Main entrypoint of the task
-		 */
-		function main()
+
+		if ($this->output)
 		{
-			$arguments = $this->constructArguments();
-			
-			$this->log("Running phpDocumentor...");
-			
-			exec(self::PHPDOC . " " . $arguments, $output, $return);
-			
-			if ($return != 0)
-			{
-				throw new BuildException("Could not execute ionCube Encoder: " . implode(' ', $output));
-			}
+			$arguments.= "-o " . $this->output . " ";
 		}
-		
-		/**
-		 * Constructs an argument string for phpDocumentor
-		 */
-		private function constructArguments()
+
+		if ($this->linksource)
 		{
-			$arguments = "-q on ";
-			
-			if ($this->title)
-			{
-				$arguments.= "-ti \"" . $this->title . "\" ";
-			}
-			
-			if ($this->destdir)
-			{
-				$arguments.= "-t " . $this->destdir . " ";
-			}
-			
-			if ($this->sourcepath !== NULL)
-			{
-				$arguments.= "-d " . $this->sourcepath->__toString() . " ";
-			}
-			
-			if ($this->output)
-			{
-				$arguments.= "-o " . $this->output . " ";
-			}
-			
-			if ($this->linksource)
-			{
-				$arguments.= "-s ";
-			}
-			
-			if ($this->parseprivate)
-			{
-				$arguments.= "-pp ";
-			}
-						
-			return $arguments;
+			$arguments.= "-s ";
 		}
-	};
+
+		if ($this->parseprivate)
+		{
+			$arguments.= "-pp ";
+		}
+
+		return $arguments;
+	}
+};
 
 ?>
