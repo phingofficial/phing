27c55e81b8352532b42c1d0ab98ec5fa516eba06
Hans Lellelid <hans@xmpl.org>
2007-07-29 21:04:42 +0000

    ticket:118 - Added fix from domain51.com for NULL projects in UnknownElement

--- classes/phing/UnknownElement.php
+++ classes/phing/UnknownElement.php
@@ -130,15 +130,19 @@ class UnknownElement extends Task {
                 $realChild = $this->makeTask($child, $childWrapper, false);
                 $parent->addTask($realChild);
             } else {
-                $realChild = $ih->createElement($this->project, $parent, $child->getTag());
+				$project = $this->project === null ? $parent->project : $this->project;
+				$realChild = $ih->createElement($project, $parent, $child->getTag());
             }
 
             $childWrapper->setProxy($realChild);
             if ($realChild instanceof Task) {
                 $realChild->setRuntimeConfigurableWrapper($childWrapper);
             }
-
-            $child->handleChildren($realChild, $childWrapper);
+			
+            if ($realChild instanceof ProjectComponent) {
+            	$child->handleChildren($realChild, $childWrapper);
+            }
+            
             if ($realChild instanceof Task) {
                 $realChild->maybeConfigure();
             }
