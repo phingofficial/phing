dadda3bb13dcd053661bf61dc0f795eec50c49ea
Hans Lellelid <hans@xmpl.org>
2007-12-20 16:05:46 +0000

    Refs #201 - Fixing another (!) argument parsing bug introduced in [326]

--- classes/phing/Phing.php
+++ classes/phing/Phing.php
@@ -319,7 +319,7 @@ class Phing {
 		$keys = array_keys($args); // Use keys and iterate to max(keys) since there may be some gaps
 		for($i=0, $max = max(array_keys($args)); $i <= $max; $i++) {
 			
-			if (!array_key_exists($i, $args[$i])) {
+			if (!array_key_exists($i, $args)) {
 				// skip this argument, since it must have been removed above.
 				continue;
 			}
