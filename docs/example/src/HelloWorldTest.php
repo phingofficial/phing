7006940ebf213d43262b6d5dd9fd96de08cfac86
Alexey Shockov <alexey@shockov.com>
2008-10-26 15:33:21 +0000

    Branching from 2.4 branch.

---
+++ docs/example/src/HelloWorldTest.php
@@ -0,0 +1,22 @@
+<?php
+
+    require_once "PHPUnit2/Framework/TestCase.php";
+    require_once "HelloWorld.php";
+
+    /**
+    * Test class for HelloWorld
+    *
+    * @author Michiel Rook
+    * @version $Id$
+    * @package hello.world
+    */
+    class HelloWorldTest extends PHPUnit2_Framework_TestCase
+    {
+        public function testSayHello()
+        {
+            $hello = new HelloWorld();
+            $this->assertEquals("Hello World!", $hello->sayHello());
+        }
+    }
+
+?>
