369a233d312d876f25ed79d56eb7cc6788f07fa8
Michiel Rook <mrook@php.net>
2008-10-28 19:14:30 +0000

    Drop PHPUnit2 compatibility

--- docs/example/src/HelloWorldTest.php
+++ docs/example/src/HelloWorldTest.php
@@ -1,6 +1,6 @@
 <?php
 
-    require_once "PHPUnit2/Framework/TestCase.php";
+    require_once "PHPUnit/Framework/TestCase.php";
     require_once "HelloWorld.php";
 
     /**
@@ -10,7 +10,7 @@
     * @version $Id$
     * @package hello.world
     */
-    class HelloWorldTest extends PHPUnit2_Framework_TestCase
+    class HelloWorldTest extends PHPUnit_Framework_TestCase
     {
         public function testSayHello()
         {
