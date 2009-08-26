128bd25e0c8a89e3cad0f59c13604c54d740212f
Michiel Rook <mrook@php.net>
2009-08-26 07:57:09 +0000

    Style fixes

--- classes/phing/tasks/ext/ZendCodeAnalyzerTask.php
+++ classes/phing/tasks/ext/ZendCodeAnalyzerTask.php
@@ -62,122 +62,130 @@ require_once 'phing/Task.php';
  * @version  $Id$
  * @package  phing.tasks.ext
  */
-class ZendCodeAnalyzerTask extends Task {
-  
-  protected $analyzerPath = ""; // Path to ZendCodeAnalyzer binary
-  protected $file = "";  // the source file (from xml attribute)
-  protected $filesets = array(); // all fileset objects assigned to this task
-  protected $warnings = array();
-  protected $counter = 0;
-  protected $disable = array();
-  protected $enable = array();
-  
-  /**
-   * File to be analyzed
-   * 
-   * @param PhingFile $file
-   */
-  public function setFile(PhingFile $file) {
-    $this->file = $file;
-  }
-  
-  /**
-   * Path to ZendCodeAnalyzer binary
-   *
-   * @param string $analyzerPath
-   */
-  public function setAnalyzerPath($analyzerPath) {
-    $this->analyzerPath = $analyzerPath;
-  }
-  
-  /**
-   * Disable warning levels. Seperate warning levels with ','
-   *
-   * @param string $disable
-   */
-  public function setDisable($disable) {
-    $this->disable = explode(",", $disable);
-  }
-  
-  /**
-   * Enable warning levels. Seperate warning levels with ','
-   *
-   * @param string $enable
-   */
-  public function setEnable($enable) {
-    $this->enable = explode(",", $enable);
-  }
-  
-  /**
-   * Nested creator, creates a FileSet for this task
-   *
-   * @return FileSet The created fileset object
-   */
-  function createFileSet() {
-    $num = array_push($this->filesets, new FileSet());
-    return $this->filesets[$num-1];
-  }
+class ZendCodeAnalyzerTask extends Task
+{
+    protected $analyzerPath = ""; // Path to ZendCodeAnalyzer binary
+    protected $file = "";  // the source file (from xml attribute)
+    protected $filesets = array(); // all fileset objects assigned to this task
+    protected $warnings = array();
+    protected $counter = 0;
+    protected $disable = array();
+    protected $enable = array();
 
-  /**
-   * Analyze against PhingFile or a FileSet
-   */
-  public function main() {
-    if(!isset($this->analyzerPath)) {
-      throw new BuildException("Missing attribute 'analyzerPath'");
+    /**
+     * File to be analyzed
+     * 
+     * @param PhingFile $file
+     */
+    public function setFile(PhingFile $file) {
+        $this->file = $file;
     }
-    if(!isset($this->file) and count($this->filesets) == 0) {
-      throw new BuildException("Missing either a nested fileset or attribute 'file' set");
-    }
-    
-    if($this->file instanceof PhingFile) {
-      $this->analyze($this->file->getPath());
-    } else { // process filesets
-      $project = $this->getProject();
-      foreach($this->filesets as $fs) {
-      	$ds = $fs->getDirectoryScanner($project);
-      	$files = $ds->getIncludedFiles();
-      	$dir = $fs->getDir($this->project)->getPath();
-      	foreach($files as $file) {
-	  $this->analyze($dir.DIRECTORY_SEPARATOR.$file);
-      	}
-      }
-    }
-    $this->log("Number of findings: ".$this->counter, Project::MSG_INFO);
-  }
 
-  /**
-   * Analyze file
-   *
-   * @param string $file
-   * @return void
-   */
-  protected function analyze($file) {
-    if(file_exists($file)) {
-      if(is_readable($file)) {
-      	
-      	// Construct shell command
-      	$cmd = $this->analyzerPath." ";
-      	foreach($this->enable as $enable) { // Enable warning levels
-      		$cmd .= " --enable $enable ";
-      	}
-      	foreach($this->disable as $disable) { // Disable warning levels
-      		$cmd .= " --disable $disable ";
-      	}
-      	$cmd .= "$file 2>&1";
-      	
-      	// Execute command
-      	$result = shell_exec($cmd);
-      	$result = explode("\n", $result);
-      	for($i=2, $size=count($result); $i<($size-1); $i++) {
-	  $this->counter++;
-	  $this->log($result[$i], Project::MSG_WARN);
-      	}
-      } else {
-      	throw new BuildException('Permission denied: '.$file);
-      }
-    } else {
-      throw new BuildException('File not found: '.$file);
+    /**
+     * Path to ZendCodeAnalyzer binary
+     *
+     * @param string $analyzerPath
+     */
+    public function setAnalyzerPath($analyzerPath) {
+        $this->analyzerPath = $analyzerPath;
     }
-  }
+
+    /**
+    * Disable warning levels. Seperate warning levels with ','
+    *
+    * @param string $disable
+    */
+    public function setDisable($disable) {
+        $this->disable = explode(",", $disable);
+    }
+
+    /**
+     * Enable warning levels. Seperate warning levels with ','
+     *
+     * @param string $enable
+     */
+    public function setEnable($enable) {
+        $this->enable = explode(",", $enable);
+    }
+
+    /**
+     * Nested creator, creates a FileSet for this task
+     *
+     * @return FileSet The created fileset object
+     */
+    function createFileSet() {
+        $num = array_push($this->filesets, new FileSet());
+        return $this->filesets[$num-1];
+    }
+
+    /**
+     * Analyze against PhingFile or a FileSet
+     */
+    public function main() {
+        if(!isset($this->analyzerPath)) {
+            throw new BuildException("Missing attribute 'analyzerPath'");
+        }
+        
+        if(!isset($this->file) and count($this->filesets) == 0) {
+            throw new BuildException("Missing either a nested fileset or attribute 'file' set");
+        }
+
+        if($this->file instanceof PhingFile) {
+            $this->analyze($this->file->getPath());
+        } else { // process filesets
+            $project = $this->getProject();
+        
+            foreach($this->filesets as $fs) {
+                $ds = $fs->getDirectoryScanner($project);
+                $files = $ds->getIncludedFiles();
+                $dir = $fs->getDir($this->project)->getPath();
+                
+                foreach($files as $file) {
+                    $this->analyze($dir.DIRECTORY_SEPARATOR.$file);
+                }
+            }
+        }
+        
+        $this->log("Number of findings: ".$this->counter, Project::MSG_INFO);
+    }
+
+    /**
+     * Analyze file
+     *
+     * @param string $file
+     * @return void
+     */
+    protected function analyze($file) {
+        if(file_exists($file)) {
+            if(is_readable($file)) {
+                // Construct shell command
+                $cmd = $this->analyzerPath." ";
+                
+                foreach($this->enable as $enable) { // Enable warning levels
+                    $cmd .= " --enable $enable ";
+                }
+                
+                foreach($this->disable as $disable) { // Disable warning levels
+                    $cmd .= " --disable $disable ";
+                }
+                
+                $cmd .= "$file 2>&1";
+
+                // Execute command
+                $result = shell_exec($cmd);
+                $result = explode("\n", $result);
+                
+                for($i=2, $size=count($result); $i<($size-1); $i++) {
+                    $this->counter++;
+                    $this->log($result[$i], Project::MSG_WARN);
+                    }
+            } 
+            else
+            {
+                throw new BuildException('Permission denied: '.$file);
+            }
+        } else {
+            throw new BuildException('File not found: '.$file);
+        }
+    }
 }
-
