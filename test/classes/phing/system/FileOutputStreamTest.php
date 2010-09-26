3398543db8065732fa90e480bf8d70b40e0041fe
Victor Farazdagi <simple.square@gmail.com>
2010-09-26 17:32:35 +0000

    Specify output directory in absolute path

--- test/classes/phing/system/FileOutputStreamTest.php
+++ test/classes/phing/system/FileOutputStreamTest.php
@@ -38,7 +38,7 @@ class FileOutputStreamTest extends PHPUnit_Framework_TestCase {
     private $outStream;
     
     public function setUp() {
-        $this->tmpFile = new PhingFile("tmp/" . get_class($this) . ".txt");
+        $this->tmpFile = new PhingFile(dirname(__FILE__) . "/../../../tmp/" . get_class($this) . ".txt");
         $this->outStream = new FileOutputStream($this->tmpFile);
     }
     
