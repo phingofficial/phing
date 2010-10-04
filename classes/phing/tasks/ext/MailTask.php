5c78c3e54311e7f3221aab4539ff65fa0bc079ab
Michiel Rook <mrook@php.net>
2010-10-04 08:45:51 +0000

    Refs #549 - revert erroneous changes

--- classes/phing/tasks/ext/MailTask.php
+++ classes/phing/tasks/ext/MailTask.php
@@ -37,16 +37,8 @@ class MailTask extends Task {
     protected $subject;
     
     protected $msg;
-    
-    protected $from = null;
-    
-    protected $filesets = array();
 
     public function main() {
-        if (empty($this->from)) {
-            throw new BuildException('Missing "from" attribute');
-        }
-        
         $this->log('Sending mail to ' . $this->recipient );    
         mail($this->recipient, $this->subject, $this->msg);
     }
@@ -80,24 +72,6 @@ class MailTask extends Task {
     public function addText($msg)
     {
         $this->msg = (string) $msg;
-    }
-    
-    /**
-     * Sets email address of sender
-     */
-    public function setFrom($from)
-    {
-        $this->from = $from;
-    }
-    
-    /**
-     * Adds a fileset
-     */
-    public function createFileSet()
-    {
-        $fileset = new FileSet();
-        $this->filesets[] = $fileset;
-        return $this->fileset;
     }
 }
 
