e64f69d9418c689df5664d6f59e745f2e758bef1
Michiel Rook <mrook@php.net>
2010-12-09 09:11:56 +0000

    Refs #598 - fix wrong version number for Services_Amazon_S3 dependency

--- build/BuildPhingPEARPackageTask.php
+++ build/BuildPhingPEARPackageTask.php
@@ -203,7 +203,7 @@ etc.), file system operations, interactive build support, SQL execution, and muc
             $package->addPackageDepWithChannel( 'optional', 'Xdebug', 'pecl.php.net', '2.0.5');
             $package->addPackageDepWithChannel( 'optional', 'Archive_Tar', 'pear.php.net', '1.3.0');
             $package->addPackageDepWithChannel( 'optional', 'PEAR_PackageFileManager', 'pear.php.net', '1.5.2');
-            $package->addPackageDepWithChannel( 'optional', 'Services_Amazon_S3', 'pear.php.net', '1.3.1');
+            $package->addPackageDepWithChannel( 'optional', 'Services_Amazon_S3', 'pear.php.net', '0.3.1');
 
             // now add the replacements, chdir() to source directory
             // to allow addReplacement() to find the specified files
