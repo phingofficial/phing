e9f357c5e4642646712edac4c279870fa05da393
Michiel Rook <mrook@php.net>
2011-01-11 13:32:55 +0000

    Refs #624 - include LogWriter class

--- classes/phing/tasks/ext/pdo/PDOSQLExecFormatterElement.php
+++ classes/phing/tasks/ext/pdo/PDOSQLExecFormatterElement.php
@@ -22,6 +22,7 @@
 require_once 'phing/system/io/PhingFile.php';
 require_once 'phing/tasks/ext/pdo/PlainPDOResultFormatter.php';
 require_once 'phing/tasks/ext/pdo/XMLPDOResultFormatter.php';
+require_once 'phing/util/LogWriter.php';
 
 /**
  * A class to represent the nested <formatter> element for PDO SQL results.
