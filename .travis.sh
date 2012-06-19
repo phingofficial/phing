c617467a002c4b57630987eaff6f021c47e6a6bf
Christoph Luehr <christoph.luehr@basilicom.de>
2012-06-19 14:50:03 +0200

    improved build environment

--- .travis.sh
+++ .travis.sh
@@ -26,6 +26,14 @@ installPearTask ()
     phing -v 2>&1 >/dev/null    &&
         echo "... OK"           ||
         return 1
+
+    sudo apt-get install python-docutils
+    pear install VersionControl_Git-alpha
+    pear install PEAR_PackageFileManager
+    pear install Net_Growl
+
+    # update paths
+    phpenv rehash
 }
 
 
@@ -35,6 +43,13 @@ installPearTask ()
         echo -e "\nSUCCESS - PHP ENVIRONMENT READY." ||
         ( echo "=== FAILED."; exit 1 )
 
-    phing $*
+    echo "=== BUILDING PHING ==="
+    cd build
+    phing -Dversion=2.0.0b1
+
+    echo "=== TESTING PHING ==="
+    cd ../test
+    ../bin/phing
+
 
 #------------------------------------------------------- eof
