b91133e623876b8a1b24c27676c283870e1e6de2
Abs <theogaden@gmail.com>
2012-12-04 15:20:57 +0000

    Undoing a change
    Undoing a change to separate commits.

--- classes/phing/tasks/ext/Service/Amazon/S3/S3PutTask.php
+++ classes/phing/tasks/ext/Service/Amazon/S3/S3PutTask.php
@@ -316,7 +316,7 @@ class S3PutTask extends Service_Amazon_S3
 		            } else {
 		                foreach ($objects as $object) {
 		                    $this->_source = $object;  	
-		                    $this->saveObject(str_replace('\\', '/', $object), file_get_contents($fromDir . DIRECTORY_SEPARATOR . $object));
+		                    $this->saveObject($object, file_get_contents($fromDir . DIRECTORY_SEPARATOR . $object));
 		                }
 		            }
 			
