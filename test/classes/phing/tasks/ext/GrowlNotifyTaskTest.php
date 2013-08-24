39e7da424bd78e6e7e46ad01373857b767e14938
Michiel Rook <mrook@php.net>
2013-08-24 15:41:58 +0200

    Check for Net_Growl

--- test/classes/phing/tasks/ext/GrowlNotifyTaskTest.php
+++ test/classes/phing/tasks/ext/GrowlNotifyTaskTest.php
@@ -65,6 +65,11 @@ class GrowlNotifyTaskTest extends BuildFileTest
      */
     public function setUp() 
     {
+        if (!class_exists('Net_Growl')) {
+            $this->markTestSkipped("Need Net_Growl installed to test");
+            return;
+        }
+        
         $this->configureProject(PHING_TEST_BASE . '/etc/tasks/ext/growl/build.xml');
 
         $name = '';
