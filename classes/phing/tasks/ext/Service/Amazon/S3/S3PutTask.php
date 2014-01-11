b0c39fd9164361034ae8d95ec8decb4f9ddaed4f
Marco Vito Moscaritolo <marco@agavee.com>
2014-01-11 20:10:45 +0100

    Added support to gszipped and max-age to S3Put task.

--- classes/phing/tasks/ext/Service/Amazon/S3/S3PutTask.php
+++ classes/phing/tasks/ext/Service/Amazon/S3/S3PutTask.php
@@ -96,7 +96,21 @@ class S3PutTask extends Service_Amazon_S3
 	 * @access protected
 	 */
 	protected $_contentType = 'binary/octet-stream';
-	
+
+    /**
+     * Object maxage (in seconds).
+     *
+     * @var int
+     */
+    protected $_maxage = null;
+
+    /**
+     * Content is gzipped.
+     *
+     * @var boolean
+     */
+    protected $_gzipped = false;
+
 	/**
 	 * Extension content type mapper
 	 *
@@ -221,6 +235,66 @@ class S3PutTask extends Service_Amazon_S3
         return (bool) $this->_createBuckets;
     }
 
+    /**
+     * Set seconds in max-age, null value exclude max-age setup.
+     *
+     * @param int $seconds
+     */
+    public function setMaxage($seconds)
+    {
+        $this->_maxage = $seconds;
+    }
+
+    /**
+     * Get seconds in max-age or null.
+     *
+     * @return int
+     *   Number of seconds in maxage or null.
+     */
+    public function getMaxage()
+    {
+        return $this->_maxage;
+    }
+
+    /**
+     * Set if content is gzipped.
+     *
+     * @param boolean $gzipped
+     */
+    public function setGzip($gzipped)
+    {
+        $this->_gzipped = $gzipped;
+    }
+
+    /**
+     * Return if content is gzipped.
+     *
+     * @return booleand
+     *   Indicate if content is gzipped.
+     */
+    public function getGzip()
+    {
+        return $this->_gzipped;
+    }
+
+    /**
+     * Generate HTTPHEader array sent to S3.
+     *
+     * @return array
+     *   HttpHeader to set in S3 Object.
+     */
+    protected function getHttpHeaders()
+    {
+        $headers = array();
+        if (!is_null($this->_maxage)) {
+            $headers['Cache-Control'] = 'max-age=' . $this->_maxage;
+        }
+        if ($this->_gzipped) {
+            $headers['Content-Encoding'] = 'gzip';
+        }
+        return $headers;
+    }
+
 	public function setFileNameOnly($fileNameOnly)
 	{
 		$this->_fileNameOnly = (bool) $fileNameOnly;
@@ -332,6 +406,7 @@ class S3PutTask extends Service_Amazon_S3
 		$object->data = $data;
 		$object->acl = $this->getAcl();
 		$object->contentType = $this->getContentType();
+		$object->httpHeaders = $this->getHttpHeaders();
 		$object->save();
 		
 		if(!$this->isObjectAvailable($object->key)) {
