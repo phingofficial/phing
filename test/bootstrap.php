6f8bfd649d289594366a302c61760ad42efc281a
mike.lohmann <mike.lohmann@bauermedia.com>
2014-01-29 14:04:14 +0100

    Added loading of autoload.php into bootstrap.php to be able to test if something needs to be loaded from vendors. Corrected config.yml for tests to meet extpectations and have correct yaml syntax. Added a array-flatten function to flat array from yaml to match the current property implementation of key => value

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -2,10 +2,15 @@
 defined('PHING_TEST_BASE') || define('PHING_TEST_BASE', dirname(__FILE__));
 set_include_path(
     realpath(dirname(__FILE__) . '/../classes') . PATH_SEPARATOR .
-    realpath(dirname(__FILE__) . '/../vendor') . PATH_SEPARATOR .
     realpath(dirname(__FILE__) . '/classes') . PATH_SEPARATOR .
     get_include_path()  // trunk version of phing classes should take precedence
 );
+// Use composers autoload.php if available
+if (file_exists(dirname(__FILE__) . '/../vendor/autoload.php')) {
+    require_once dirname(__FILE__) . '/../vendor/autoload.php';
+} else if (file_exists(dirname(__FILE__) . '/../../../autoload.php')) {
+    require_once dirname(__FILE__) . '/../../../autoload.php';
+}
 
 require_once(dirname(__FILE__) . '/classes/phing/BuildFileTest.php');
 require_once('phing/Phing.php');
