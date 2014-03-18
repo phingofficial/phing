edd92b12fba4e0141075c5f65acd8e4c7bdfcb7a
Philipp Dobrigkeit <p.dobrigkeit@goalio.de>
2014-03-18 16:13:38 +0100

    Update DbDeployTask.php
    Remove debug echo

--- classes/phing/tasks/ext/dbdeploy/DbDeployTask.php
+++ classes/phing/tasks/ext/dbdeploy/DbDeployTask.php
@@ -241,8 +241,6 @@ class DbDeployTask extends Task
      */
     protected function generateSql($undo = false)
     {
-        echo 'narf';
-
         $sql = '';
         $lastChangeAppliedInDb = $this->getLastChangeAppliedInDb();
         $files = $this->getDeltasFilesArray();
