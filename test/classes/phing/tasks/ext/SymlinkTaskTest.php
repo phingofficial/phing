cffe339c1de0f43c9b4c89957bf82f4e0d948c41
Michiel Rook <mrook@php.net>
2014-08-06 15:14:00 +0200

    Add a few tests for the symlink task

---
+++ test/classes/phing/tasks/ext/SymlinkTaskTest.php
@@ -0,0 +1,76 @@
+<?php
+/*
+ *  $Id$
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information please see
+ * <http://phing.info>.
+ */
+
+require_once 'phing/BuildFileTest.php';
+
+/**
+ * Tests the Available Task
+ *
+ * @author  Michiel Rook <mrook@php.net>
+ * @version $Id$
+ * @package phing.tasks.system
+ */
+class SymlinkTaskTest extends BuildFileTest
+{
+    public function setUp()
+    {
+        if (strtoupper(substr(PHP_OS, 0, 3)) == 'WIN') {
+            $this->markTestSkipped("Symlinks don't work on Windows");
+        }
+
+        $this->configureProject(
+            PHING_TEST_BASE
+            . "/etc/tasks/ext/SymlinkTaskTest.xml"
+        );
+        $this->executeTarget("setup");
+    }
+
+    public function tearDown()
+    {
+        $this->executeTarget("clean");
+    }
+
+    public function testSymlinkExists()
+    {
+        $this->executeTarget(__FUNCTION__);
+        $this->assertInLogs("Link exists: ");
+    }
+
+    public function testOverwritingSymlink()
+    {
+        $this->executeTarget(__FUNCTION__);
+        $this->assertEquals("fake2", readlink(PHING_TEST_BASE . "/etc/tasks/ext/tmp/l"));
+        $this->assertInLogs("Link removed: ");
+    }
+
+    public function testOverwritingDirectory()
+    {
+        $this->executeTarget(__FUNCTION__);
+        $this->assertEquals("fake1", readlink(PHING_TEST_BASE . "/etc/tasks/ext/tmp/l"));
+        $this->assertInLogs("Directory removed: ");
+    }
+
+    public function testNotOverwritingSymlink()
+    {
+        $this->executeTarget(__FUNCTION__);
+        $this->assertEquals("fake1", readlink(PHING_TEST_BASE . "/etc/tasks/ext/tmp/l"));
+    }
+}
