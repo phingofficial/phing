19feb504695cb2317667e235d096462eaedb5701
siad007 <siad.ardroumli@googlemail.com>
2014-10-12 11:20:30 +0200

    Added HttpCondition

--- classes/phing/tasks/system/condition/ConditionBase.php
+++ classes/phing/tasks/system/condition/ConditionBase.php
@@ -193,6 +193,14 @@ abstract class ConditionBase extends ProjectComponent
         return $this->conditions[$num - 1];
     }
 
+    public function createHttp()
+    {
+        include_once 'phing/tasks/system/condition/HttpCondition.php';
+        $num = array_push($this->conditions, new HttpCondition());
+
+        return $this->conditions[$num - 1];
+    }
+
     /**
      * @param  string         $elementName
      * @param  Project        $project
