93d8df3af5de89fca91b68be26be6ccc86b1464a
Michiel Rook <mrook@php.net>
2014-12-02 22:06:27 +0100

    Uncomment code

--- classes/phing/types/Path.php
+++ classes/phing/types/Path.php
@@ -378,13 +378,11 @@ class Path extends DataType
                 }
                 $result[] = $element;
             } catch (BuildException $e) {
-                /*
                 $this->project->log(
                     "Dropping path element " . $pathElement
                     . " as it is not valid relative to the project",
                     Project::MSG_VERBOSE
                 );
-                */
             }
         }
 
