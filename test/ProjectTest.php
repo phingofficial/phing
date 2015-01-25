814366cd7bd61565162ccd89b9ca47ba6243690a
Matthias Pigulla <mp@webfactory.de>
2015-01-25 22:33:08 +0100

    Mark test as skipped, behaviour seems to be not supported at the moment

--- test/ProjectTest.php
+++ test/ProjectTest.php
@@ -44,6 +44,7 @@ class ProjectTest extends \PHPUnit_Framework_TestCase
 
     public function testGetPropertiesWillExpandPropertyValues()
     {
+        $this->markTestSkipped('This is currently not supported in Phing but might change with https://github.com/phingofficial/phing/pull/363');
         $this->project->setProperty('foo', 'foo');
         $this->project->setProperty('bar', '${foo}bar');
 
