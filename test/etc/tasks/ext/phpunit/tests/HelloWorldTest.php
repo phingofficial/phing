6e7350ba2756bd7d1ad1fa5b5a2ac1c8139ac3f8
Joeri van Oostveen <joeri@klieder.net>
2015-01-27 21:16:48 +0100

    Added the base path for requiring the to be tested file.

--- test/etc/tasks/ext/phpunit/tests/HelloWorldTest.php
+++ test/etc/tasks/ext/phpunit/tests/HelloWorldTest.php
@@ -1,21 +1,18 @@
 <?php
 
-    require_once "etc/tasks/ext/phpunit/src/HelloWorld.php";
+require_once PHING_TEST_BASE . '/etc/tasks/ext/phpunit/src/HelloWorld.php';
 
-    /**
-    * Test class for HelloWorld
-    *
-    * @author Michiel Rook
-    * @version $Id$
-    * @package hello.world
-    */
-    class HelloWorldTest extends PHPUnit_Framework_TestCase
+/**
+ * Test class for HelloWorld
+ *
+ * @author Michiel Rook
+ * @package hello.world
+ */
+class HelloWorldTest extends PHPUnit_Framework_TestCase
+{
+    public function testSayHello()
     {
-        public function testSayHello()
-        {
-            $hello = new HelloWorld();
-            $this->assertEquals("Hello World!", $hello->sayHello());
-        }
+        $hello = new HelloWorld();
+        $this->assertEquals('Hello World!', $hello->sayHello());
     }
-
-?>
+}
