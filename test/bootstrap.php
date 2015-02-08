160eba636898037615e496e87a3aa70fda857e8a
Matthias Pigulla <mp@webfactory.de>
2015-02-08 21:32:01 +0100

    Add a constant and Property for the tmp/ dir during tests

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -5,6 +5,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Phing\Phing;
 
 defined('PHING_TEST_BASE') || define('PHING_TEST_BASE', __DIR__);
+defined('PHING_TEST_TMP') || define('PHING_TEST_TMP', __DIR__.'/tmp');
+
 set_include_path(
     realpath(__DIR__ . '/../classes') . PATH_SEPARATOR . 
     realpath(__DIR__ . '/classes') . PATH_SEPARATOR .
@@ -12,6 +14,8 @@ set_include_path(
 );
 
 Phing::setProperty('phing.home', realpath(__DIR__ . '/../'));
+Phing::setProperty('phing.test_tmp', PHING_TEST_TMP);
+Phing::setProperty('tmp.dir', PHING_TEST_TMP); // already used by some existing tests
 Phing::startup();
 
 error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE & ~E_STRICT);
