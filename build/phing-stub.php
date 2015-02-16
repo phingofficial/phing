632831976269de903012838230bee7e381bb2d12
Laurent Laville <laurent.laville@gmail.com>
2015-02-16 16:53:24 +0100

    solve ticket #1134 and add color support on phar version (all platforms)

--- build/phing-stub.php
+++ build/phing-stub.php
@@ -1,7 +1,17 @@
 #!/usr/bin/env php
 <?php
 
-if (DIRECTORY_SEPARATOR != '\\' && function_exists('posix_isatty') && @posix_isatty(STDOUT)) {
+/**
+ * Code form Symfony/Component/Console/Output/StreamOutput.php
+ */
+function hasColorSupport() {
+    if (DIRECTORY_SEPARATOR == '\\') {
+        return false !== getenv('ANSICON') || 'ON' === getenv('ConEmuANSI');
+    }
+    return function_exists('posix_isatty') && @posix_isatty(STDOUT);
+}
+
+if (hasColorSupport()) {
     array_push($argv, '-logger');
     array_push($argv, 'phing.listener.AnsiColorLogger');
     $argc+=2;
