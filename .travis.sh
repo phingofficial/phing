f9d52afb0f4695ceadd33bce4fe6bd1076ea2e38
Michiel Rook <mrook@php.net>
2016-10-05 22:51:34 +0200

    Disable checks for PHP < 5.5

--- .travis.sh
+++ .travis.sh
@@ -14,40 +14,9 @@
         echo "... OK"
     fi
     
-    if [[ $TRAVIS_PHP_VERSION < 5.3 ]]; then
-        pear upgrade pecl.php.net/Phar ||
-            pear install pecl.php.net/Phar
-
-        echo -e "\nInstalling / upgrading phpcs ... "
-        which phpcs >/dev/null                             &&
-            pear upgrade pear.php.net/PHP_CodeSniffer ||
-            pear install pear.php.net/PHP_CodeSniffer
-        phpenv rehash
-        # re-test for phpcs:
-        phpcs --version 2>&1 >/dev/null   &&
-            echo "... OK"
-            
-        echo -e "\nInstalling / upgrading phpdepend ... "
-        which pdepend >/dev/null                      &&
-            pear upgrade pear.pdepend.org/PHP_Depend-1.1.0 ||
-            pear install pear.pdepend.org/PHP_Depend-1.1.0
-        
-        echo -e "\nInstalling PEAR packages ... "
-        pear install pear/XML_Serializer-beta
-        pear install --alldeps PEAR_PackageFileManager
-        pear install --alldeps PEAR_PackageFileManager2
-        pear install Net_Growl
-        pear install HTTP_Request2
-        pear install VersionControl_SVN-alpha
-        pear install VersionControl_Git-alpha
-        
-        mkdir vendor
-        touch vendor/autoload.php
-    else
-    	echo -e "\nInstalling composer packages ... "
-    	composer selfupdate --quiet
-        composer install -o --no-progress --prefer-dist
-    fi
+    echo -e "\nInstalling composer packages ... "
+    composer selfupdate --quiet
+    composer install -o --no-progress --prefer-dist
 
     if [[ $TRAVIS_PHP_VERSION != 'hhvm-nightly' && $TRAVIS_PHP_VERSION != 'hhvm' ]]; then
         phpenv config-add .travis.php.ini
