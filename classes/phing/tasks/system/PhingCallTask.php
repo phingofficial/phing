9fe16c4dd7b15c6f956b78d24685beedf58296b1
Michiel Rook <mrook@php.net>
2016-10-07 12:47:47 +0200

    Disallow phingcall's from the project root

--- classes/phing/tasks/system/PhingCallTask.php
+++ classes/phing/tasks/system/PhingCallTask.php
@@ -151,6 +151,10 @@ class PhingCallTask extends Task
      */
     public function main()
     {
+        if ($this->getOwningTarget()->getName() === "") {
+            $this->log("Cowardly refusing to call target '{$this->subTarget}' from the root", Project::MSG_WARN);
+            return;
+        }
 
         $this->log("Running PhingCallTask for target '" . $this->subTarget . "'", Project::MSG_DEBUG);
         if ($this->callee === null) {
