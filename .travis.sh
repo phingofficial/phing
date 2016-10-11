569470460dab2ac1a51327755f51d91566e36664
Michiel Rook <mrook@php.net>
2016-10-11 10:41:33 +0200

    Try ignoring platform reqs to get build going

--- .travis.sh
+++ .travis.sh
@@ -46,7 +46,7 @@
     else
     	echo -e "\nInstalling composer packages ... "
     	composer selfupdate --quiet
-        composer install -o --no-progress --prefer-dist
+        composer install -o --no-progress --prefer-dist --ignore-platform-reqs
     fi
 
     if [[ $TRAVIS_PHP_VERSION != 'hhvm-nightly' && $TRAVIS_PHP_VERSION != 'hhvm' ]]; then
