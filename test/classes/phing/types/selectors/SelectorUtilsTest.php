60c43e249ff638d73a632ac6468f53d2a01391b8
Michiel Rook <mrook@php.net>
2016-10-12 14:54:02 +0200

    #593 - include paths in base of fileset

--- test/classes/phing/types/selectors/SelectorUtilsTest.php
+++ test/classes/phing/types/selectors/SelectorUtilsTest.php
@@ -18,11 +18,25 @@ class SelectorUtilsTest extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * Inspired by @link http://www.phing.info/trac/ticket/796
+     * Inspired by @link https://www.phing.info/trac/ticket/796
      */
     public function testDoNotIncludeSelfWhenMatchingSubdirectoriesAndFiles()
     {
         $result = $this->selectorUtils->matchPath("**/*", "");
         $this->assertFalse($result);
+    }
+
+    /**
+     * Inspired by @link https://www.phing.info/trac/ticket/1264
+     */
+    public function testDoNotIncludePrefix() {
+        $this->assertFalse($this->selectorUtils->matchPath("**/example.php", "vendor/phplot/phplot/contrib/color_range.example.php"));
+    }
+
+    /**
+     * Inspired by @link https://github.com/phingofficial/phing/issues/593
+     */
+    public function testIncludePathsInBase() {
+        $this->assertTrue($this->selectorUtils->matchPath("**/domain.ext/**", "domain.ext/foo"));
     }
 }
