c6a42955c3ca98a37cc64b80b77ad75dfe00d67e
Tom Klingenberg <tklingenberg@lastflood.net>
2016-12-18 10:56:25 +0100

    Typo change execption to exception (#634)
    Minor mistype/swap of characters. Became aware of in one's of our own
    projects in which phing was used so caught these as by-catch.
    
    Via command line:
    
      find . -type f -name '*.php' -exec grep -q execption {} \; \
        -exec sed -i 's/execption/exception/g' {} \; -print
    
      ./classes/phing/BuildEvent.php
      ./classes/phing/parser/ProjectConfigurator.php

--- classes/phing/BuildEvent.php
+++ classes/phing/BuildEvent.php
@@ -77,7 +77,7 @@ class BuildEvent extends EventObject
     protected $priority = Project::MSG_VERBOSE;
 
     /**
-     * The execption that caused the event, if any
+     * The exception that caused the event, if any
      *
      * @var    object
      */
