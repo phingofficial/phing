a7826e4a1784496b5e6f347751f71b339f5dac09
cdburgess@gmail.com <cdburgess@gmail.com>
2016-12-21 20:37:35 -0700

    RuntimeException Wrapper
    - creating the Phing namespace RuntimeException wrapper

---
+++ classes/phing/RuntimeException.php
@@ -0,0 +1,13 @@
+<?php namespace Phing;
+
+/**
+ * BuildException is for when things go wrong in a build execution.
+ *
+ * @author   Andreas Aderhold <andi@binarycloud.com>
+ * @version  $Id$
+ * @package  phing
+ */
+class RuntimeException extends \RuntimeException
+{
+
+}
