e5e462dc2cea0e1ba4024413f93db6d61a031c4a
siad007 <siad.ardroumli@gmail.com>
2017-02-26 02:31:39 +0100

    Refactored getRef using getCheckedRef
    (cherry picked from commit 9e3e51f)

--- classes/phing/tasks/ext/ssh/Ssh2MethodParam.php
+++ classes/phing/tasks/ext/ssh/Ssh2MethodParam.php
@@ -189,16 +189,7 @@ class Ssh2MethodParam extends DataType
      */
     public function getRef(Project $p)
     {
-        if (!$this->checked) {
-            $stk = [];
-            array_push($stk, $this);
-            $this->dieOnCircularReference($stk, $p);
-        }
-        $o = $this->ref->getReferencedObject($p);
-        if (!($o instanceof Ssh2MethodParam)) {
-            throw new BuildException($this->ref->getRefId() . " doesn't denote a Ssh2MethodParam");
-        } else {
-            return $o;
-        }
+        $dataTypeName = StringHelper::substring(get_class(), strrpos(get_class(), '\\') + 1);
+        return $this->getCheckedRef(get_class(), $dataTypeName);
     }
 }
