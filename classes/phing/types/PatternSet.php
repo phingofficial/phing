1da6833db863a2e1e8f234499bc53744f7b9a34f
Siad Ardroumli <siad.ardroumli@googlemail.com>
2017-03-21 14:07:14 +0100

    ForeachTask and Reference additions Related to #649 (#679)

--- classes/phing/types/PatternSet.php
+++ classes/phing/types/PatternSet.php
@@ -345,18 +345,8 @@ class PatternSet extends DataType
      */
     public function getRef(Project $p)
     {
-        if (!$this->checked) {
-            $stk = [];
-            array_push($stk, $this);
-            $this->dieOnCircularReference($stk, $p);
-        }
-        $o = $this->ref->getReferencedObject($p);
-        if (!($o instanceof PatternSet)) {
-            $msg = $this->ref->getRefId() . " doesn't denote a patternset";
-            throw new BuildException($msg);
-        } else {
-            return $o;
-        }
+        $dataTypeName = StringHelper::substring(get_class(), strrpos(get_class(), '\\') + 1);
+        return $this->getCheckedRef(get_class(), $dataTypeName);
     }
 
     /**
