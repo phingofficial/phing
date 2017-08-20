45dd47780cd5f7598b912005d539a1c5e881627d
siad007 <siad.ardroumli@gmail.com>
2017-08-20 22:27:58 +0200

    Added location setting to all task defined.

--- classes/phing/TaskAdapter.php
+++ classes/phing/TaskAdapter.php
@@ -44,6 +44,16 @@ class TaskAdapter extends Task
      */
     public function main()
     {
+        if (method_exists($this->proxy, 'setLocation')) {
+            try { // try to set location
+                $this->proxy->setLocation($this->getLocation());
+            } catch (Exception $ex) {
+                $this->log('Error setting location in ' . get_class($this->proxy) . Project::MSG_ERR);
+                throw new BuildException($ex);
+            }
+        } else {
+            throw new Exception('Error setting location in class ' . get_class($this->proxy));
+        }
         if (method_exists($this->proxy, "setProject")) {
             try { // try to set project
                 $this->proxy->setProject($this->project);
