d03a227993482eedcf8a5889ea20f708a8793071
Siad Ardroumli <siad.ardroumli@googlemail.com>
2018-04-02 12:24:53 +0200

    Removed obsolete php version check

--- classes/phing/system/io/FileParserFactory.php
+++ classes/phing/system/io/FileParserFactory.php
@@ -1,6 +1,5 @@
 <?php
-/*
- *
+/**
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -42,17 +41,13 @@ class FileParserFactory implements FileParserFactoryInterface
      */
     public function createParser($fileExtension)
     {
-        if (PHP_VERSION >= 5.3) {
-            switch ($fileExtension) {
-                case self::YAMLFILEEXTENSION:
-                case self::YAMLFILEEXTENSIONLONG:
-                    $fileParser = new YamlFileParser();
-                    break;
-                default:
-                    $fileParser = new IniFileParser();
-            }
-        } else {
-            $fileParser = new IniFileParser();
+        switch ($fileExtension) {
+            case self::YAMLFILEEXTENSION:
+            case self::YAMLFILEEXTENSIONLONG:
+                $fileParser = new YamlFileParser();
+                break;
+            default:
+                $fileParser = new IniFileParser();
         }
 
         return $fileParser;
