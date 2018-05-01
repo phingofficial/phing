42f980dccb97c90a640213580e9d619d973b01a4
Maciej Lew <3950004+maciejlew@users.noreply.github.com>
2018-05-02 00:48:39 +0200

    PHPStan task (#908)
    * Adds PHPStan task fascade
    
    * Adds PHPStan command builders factory
    
    * Moves PHPStan command builders factory test to subdirectory
    
    * Implements PHPStan command builders
    
    * Adds PHPStan task
    
    * Fixes PHPStan test build file
    
    * Adds PHPStan task docs

---
+++ test/classes/phing/tasks/ext/PHPStanTask/Unit/CommandBuilder/PHPStanAnalyseCommandBuilderTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class PHPStanAnalyseCommandBuilderTest extends TestCase
+{
+
+    /** @var PHPStanAnalyseCommandBuilder */
+    private $builder;
+
+    public function setUp()
+    {
+        $this->builder = new PHPStanAnalyseCommandBuilder();
+    }
+
+    public function testItHandlesCommandOptions(): void
+    {
+        $task = new PHPStanTask();
+        $task->setExecutable('phpstan');
+        $task->setCommand('analyse');
+
+        $task->setConfiguration('anyConfiguration');
+        $task->setLevel('anyLevel');
+        $task->setNoProgress(true);
+        $task->setDebug(true);
+        $task->setAutoloadFile('anyAutoloadFile');
+        $task->setErrorFormat('anyErrorFormat');
+        $task->setMemoryLimit('anyMemoryLimit');
+        $task->setPaths('path1 path2');
+
+        $command = $this->builder->build($task);
+
+        $expectedCommand = 'phpstan analyse';
+        $expectedCommand .= ' --configuration=anyConfiguration';
+        $expectedCommand .= ' --level=anyLevel';
+        $expectedCommand .= ' --no-progress';
+        $expectedCommand .= ' --debug';
+        $expectedCommand .= ' --autoload-file=anyAutoloadFile';
+        $expectedCommand .= ' --errorFormat=anyErrorFormat';
+        $expectedCommand .= ' --memory-limit=anyMemoryLimit';
+        $expectedCommand .= ' path1 path2';
+
+        $this->assertEquals($expectedCommand, $command);
+    }
+}
