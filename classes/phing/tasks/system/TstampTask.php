70f9082ddec21a400516942fadbc2e7d395c290a
siad007 <siad.ardroumli@gmail.com>
2018-11-18 20:30:23 +0100

    Fixed method call.

--- classes/phing/tasks/system/TstampTask.php
+++ classes/phing/tasks/system/TstampTask.php
@@ -69,7 +69,7 @@ class TstampTask extends Task
         $d = $this->getNow();
 
         foreach ($this->customFormats as $cf) {
-            $cf->execute($this, $d, $this->getLocation());
+            $cf->execute($this);
         }
 
         $dstamp = strftime('%Y%m%d', $d);
