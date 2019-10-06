7082f0f8f9769f8860d63f455dfd610f606fc729
Siad Ardroumli <siad.ardroumli@gmail.com>
2019-10-06 14:03:06 +0200

    [PhingTest] Added test case for printUsage (#1149)

--- test/classes/phing/PhingTest.php
+++ test/classes/phing/PhingTest.php
@@ -112,6 +112,14 @@ class PhingTest extends \PHPUnit\Framework\TestCase
         $this->assertNull($phing->printTargets($project));
     }
 
+    public function testPrintUsage(): void
+    {
+        $phing = new Phing();
+        $phing::setErrorStream($this->getMockBuilder(OutputStream::class)->disableOriginalConstructor()->getMock());
+
+        $this->assertNull($phing::printUsage());
+    }
+
     public function testCallStartupShutdown()
     {
         $this->assertNull(Phing::startup());
