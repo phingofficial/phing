12cc7afb35449259779e1c1f47cab8ae768e0d82
Ken Guest <kguest@php.net>
2019-10-07 23:07:15 +0100

    Test setPath also

--- test/classes/phing/types/CommandlineArgumentTest.php
+++ test/classes/phing/types/CommandlineArgumentTest.php
@@ -18,4 +18,27 @@ class CommandlineArgumentTest extends \PHPUnit\Framework\TestCase
             $this->assertEquals(false, $argument->escape);
         }
     }
+
+    public function testSetEscape()
+    {
+        $command = "usblamp -s -r 5 red green blue off";
+        $commandline = new Commandline($command);
+        $argument = new CommandlineArgument($commandline);
+        $this->assertEquals($argument->escape, false);
+        $argument->setEscape(true);
+        $this->assertEquals($argument->escape, true);
+    }
+
+    public function testSetline()
+    {
+        $commandline = new Commandline($command);
+        $argument = new CommandlineArgument($commandline);
+        $argument->setLine(null);
+        $parts = $argument->getParts();
+        $this->assertEquals($parts, []);
+        $argument->setLine("perl -pie 's/foo/bar/g' test.txt");
+        $parts = $argument->getParts();
+        $this->assertNotEquals($parts, []);
+
+    }
 }
