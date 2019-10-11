6959c2f3e25c3220712d20443136ed694bf2f25e
Ken Guest <kguest@php.net>
2019-10-11 21:53:03 +0100

    more PatternSet unit tests

--- test/classes/phing/types/PatternSetTest.php
+++ test/classes/phing/types/PatternSetTest.php
@@ -38,4 +38,29 @@ class PatternSetTest extends \PHPUnit\Framework\TestCase
         $this->patternset->setRefId($reference);
         $this->patternset->createExclude();
     }
+
+    /**
+     * @expectedException BuildException
+     * @expectedExceptionMessage You must not specify nested elements when using refid
+     */
+    public function testIfReferencesSetThenCreatExcludesFileThrowsException()
+    {
+        $project = new Project();
+        $reference = new Reference($project);
+        $this->patternset->setRefId($reference);
+        $this->patternset->createExcludesFile();
+    }
+
+    /**
+     * @expectedException BuildException
+     * @expectedExceptionMessage You must not specify nested elements when using refid
+     */
+    public function testIfReferencesSetThenCreatIncludesFileThrowsException()
+    {
+        $project = new Project();
+        $reference = new Reference($project);
+        $this->patternset->setRefId($reference);
+        $this->patternset->createIncludesFile();
+    }
+
 }
