ebe2624705dcef9b13a58d1c057c17fdafe74343
Ken Guest <ken@linux.ie>
2019-10-18 09:05:23 +0100

    Unit tests for Environment and EnvVariable types. (#1157)

---
+++ test/classes/phing/types/EnvironmentTest.php
@@ -0,0 +1,43 @@
+<?php
+
+class DataTypeTest extends \PHPUnit\Framework\TestCase
+{
+    private $environment;
+
+    public function setUp(): void
+    {
+        $this->environment = new Environment;
+    }
+
+    public function testVariablesNull()
+    {
+        $count = $this->environment->getVariables();
+        $this->assertNull($count);
+    }
+
+    public function testVariablesObjectIsArrayObject()
+    {
+        $variablesObj = $this->environment->getVariablesObject();
+        $this->assertEquals("ArrayObject", get_class($variablesObj));
+    }
+
+    /**
+     * @expectedException        BuildException
+     * @expectedExceptionMessage key and value must be specified for environment variables.
+     */
+    public function testValidateWithoutKeyAndValueSetRaisesException()
+    {
+        $ev = new EnvVariable();
+        $ev->validate();
+    }
+
+    public function testValuesAgainstGetContent()
+    {
+        $ev = new EnvVariable();
+        $ev->setKey(" key ");
+        $ev->setValue(" value ");
+        $ev->validate();
+        $content = $ev->getContent();
+        $this->assertEquals("key=value", $content);
+    }
+}
