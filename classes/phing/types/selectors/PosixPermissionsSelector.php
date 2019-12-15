418ff8d0431a8a83eb00ca5c6f04976b76fcef34
siad007 <siad.ardroumli@gmail.com>
2019-12-15 23:50:30 +0100

    Added posix permission selector

---
+++ classes/phing/types/selectors/PosixPermissionsSelector.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information please see
+ * <http://phing.info>.
+ */
+
+/**
+ * A selector that selects files based on their POSIX permissions.
+ *
+ * @author  Siad Ardroumli <siad.ardroumli@gmail.com>
+ * @package phing.types.selectors
+ */
+class PosixPermissionsSelector implements FileSelector
+{
+    /** @var string $permissions */
+    private $permissions;
+
+    /**
+     * Sets the permissions to look for.
+     * @param string $permissions the permissions string (rwxrwxrwx or octal)
+     */
+    public function setPermissions($permissions): void
+    {
+        $this->validate($permissions);
+
+        if (strlen($permissions) === 3 && ctype_digit($permissions)) {
+            $this->permissions = $permissions;
+            return;
+        }
+
+        $this->permissions = implode(
+            '',
+            array_map(
+                'array_sum',
+                array_chunk(
+                    str_split(
+                        strtr(
+                            $permissions,
+                            array_combine(
+                                ['r', 'w', 'x', '-'],
+                                [4, 2, 1, 0]
+                            )
+                        )
+                    ),
+                    3
+                )
+            )
+        );
+    }
+
+    private function validate(string $permissions): void
+    {
+        if (
+            preg_match('/^[0-7]{3}$/', $permissions) !== 1 &&
+            preg_match('/^[r-][w-][x-][r-][w-][x-][r-][w-][x-]$/', $permissions) !== 1
+        ) {
+            throw new BuildException("the permissions attribute {$permissions} is invalid");
+        }
+    }
+
+    public function isSelected(PhingFile $basedir, $filename, PhingFile $file)
+    {
+        if ($this->permissions === null) {
+            throw new BuildException('the permissions attribute is required');
+        }
+
+        return (fileperms($file->getPath()) & 0777) === $this->permissions;
+    }
+}
---
+++ test/classes/phing/types/selectors/PosixPermissionsSelectorTest.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information please see
+ * <http://phing.info>.
+ */
+
+use PHPUnit\Framework\TestCase;
+use OsCondition as Os;
+
+/**
+ * Class PosixPermissionsSelectorTest
+ */
+class PosixPermissionsSelectorTest extends TestCase
+{
+    /** @var PosixPermissionsSelector $selector */
+    private $selector;
+
+    protected function setUp(): void
+    {
+        if (!Os::isFamily(Os::FAMILY_UNIX)) {
+            $this->markTestSkipped('Not POSIX');
+        }
+
+        $this->selector = new PosixPermissionsSelector();
+    }
+
+    /**
+     * @test
+     */
+    public function argumentRequired(): void
+    {
+        $this->expectException(BuildException::class);
+        $this->expectExceptionMessage('the permissions attribute is required');
+
+        $file = $this->getMockBuilder(PhingFile::class)->disableAutoload()->disableOriginalClone()->getMock();
+        $this->selector->isSelected($file, '', $file);
+    }
+
+    /**
+     * @test
+     */
+    public function isSelected()
+    {
+        $this->selector->setPermissions('rw-r--r--');
+        $this->assertTrue(
+            $this->selector->isSelected(
+                new PhingFile(__DIR__),
+                (new PhingFile(__FILE__))->getName(),
+                new PhingFile(__FILE__)
+            )
+        );
+    }
+
+    /**
+     * @test
+     * @dataProvider illegalArgumentProvider
+     * @dataProvider legalArgumentProvider
+     * @param string $permission
+     * @param bool $throws
+     */
+    public function argument(string $permission, $throws = false): void
+    {
+        if ($throws) {
+            $this->expectException(BuildException::class);
+        } else {
+            $this->expectNotToPerformAssertions();
+        }
+
+        $this->selector->setPermissions($permission);
+    }
+
+    public function legalArgumentProvider(): array
+    {
+        return [
+            'legal octal string' => ['750'],
+            'legal posix string' => ['rwxr-x---'],
+        ];
+    }
+
+    public function illegalArgumentProvider(): array
+    {
+        return [
+            ['855', true],
+            ['4555', true],
+            ['-rwxr-xr-x', true],
+            ['xrwr-xr-x', true],
+        ];
+    }
+}
