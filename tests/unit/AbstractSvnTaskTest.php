ad284c8667c482a48cec37b3c83e2df4d42e5a40
siad007 <siad.ardroumli@gmail.com>
2020-01-01 23:26:04 +0100

    Added test files

---
+++ tests/unit/AbstractSvnTaskTest.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information please see
+ * <http://phing.info>.
+ */
+
+namespace Phing\Test\Tasks\Ext;
+
+/**
+ * @author Michiel Rook <mrook@php.net>
+ * @package phing.tasks.ext
+ */
+abstract class AbstractSvnTaskTest extends AbstractBuildFileTest
+{
+    protected $backupGlobals = false;
+
+    protected $savedErrorLevel = 0;
+
+    protected function initialize($buildFilename, $createDirectory = true)
+    {
+        if (is_readable(PHING_TEST_BASE . '/tmp/svn')) {
+            // make sure we purge previously created directory
+            // if left-overs from previous run are found
+            $this->rmdir(PHING_TEST_BASE . '/tmp/svn');
+        }
+
+        if ($createDirectory) {
+            // set temp directory used by test cases
+            mkdir(PHING_TEST_BASE . '/tmp/svn');
+        }
+
+        $this->savedErrorLevel = error_reporting();
+        error_reporting(E_ERROR);
+
+        $this->configureProject(
+            PHING_TEST_BASE
+            . '/etc/' . $buildFilename
+        );
+    }
+
+    public function tearDown(): void
+    {
+        error_reporting($this->savedErrorLevel);
+        $this->rmdir(PHING_TEST_BASE . '/tmp/svn');
+    }
+}
---
+++ tests/unit/SvnSwitchTaskTest.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information please see
+ * <http://phing.info>.
+ */
+
+namespace Phing\Test\Tasks\Ext;
+
+/**
+ * @author Michiel Rook <mrook@php.net>
+ * @package phing.tasks.ext
+ */
+class SvnSwitchTaskTest extends AbstractBuildFileTest
+{
+    use SvnTaskTestSkip;
+
+    public function setUp(): void
+    {
+        $this->markTestAsSkippedWhenSvnNotInstalled();
+
+        if (is_readable(PHING_TEST_BASE . '/tmp/svn')) {
+            // make sure we purge previously created directory
+            // if left-overs from previous run are found
+            $this->rmdir(PHING_TEST_BASE . '/tmp/svn');
+        }
+        // set temp directory used by test cases
+        mkdir(PHING_TEST_BASE . '/tmp/svn');
+
+        $this->configureProject(
+            PHING_TEST_BASE
+            . '/etc/SvnSwitchTest.xml'
+        );
+    }
+
+    public function tearDown(): void
+    {
+        $this->rmdir(PHING_TEST_BASE . '/tmp/svn');
+    }
+
+    public function testSwitchSimple()
+    {
+        $repository = PHING_TEST_BASE . DIRECTORY_SEPARATOR . 'tmp' . DIRECTORY_SEPARATOR . 'svn';
+        $this->executeTarget('switchSimple');
+        $this->assertInLogs("Checking out SVN repository to '" . $repository . "'");
+        $this->assertInLogs(
+            "Switching SVN repository at '$repository' to 'https://github.com/phingofficial/phing/tags/2.10.0/etc'"
+        );
+    }
+}
