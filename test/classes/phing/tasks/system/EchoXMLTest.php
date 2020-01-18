0c4950263f7f715170415899815819e116b7d4a6
siad007 <siad.ardroumli@gmail.com>
2020-01-18 19:07:08 +0100

    Added test

---
+++ test/classes/phing/tasks/system/EchoXMLTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information please see
+ * <http://phing.info>.
+ */
+
+/**
+ * Tests the EchoXML Task
+ *
+ * @author Siad Ardroumli <siad.ardroumli@gmail.com>
+ * @package phing.tasks.system
+ */
+class EchoXMLTest extends BuildFileTest
+{
+    public function setUp(): void
+    {
+        $this->configureProject(
+            PHING_TEST_BASE . '/etc/tasks/system/echoxml.xml'
+        );
+    }
+
+    public function tearDown(): void
+    {
+        $this->getProject()->executeTarget('tearDown');
+    }
+
+    public function testPass(): void
+    {
+        $this->getProject()->executeTarget('testPass');
+    }
+
+    public function testFail(): void
+    {
+        $this->expectBuildException('testFail', '${foo}=bar');
+    }
+
+    public function testEmpty(): void
+    {
+        $this->expectBuildException('testEmpty', 'No nested XML specified');
+    }
+}
