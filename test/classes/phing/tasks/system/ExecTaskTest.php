e3c367933116c433ad5057a04b11002f60adacf8
Siad Ardroumli <siad.ardroumli@gmail.com>
2020-02-09 01:19:02 +0100

    [ExecTask] Fixed resolving env vars (#1254)

--- test/classes/phing/tasks/system/ExecTaskTest.php
+++ test/classes/phing/tasks/system/ExecTaskTest.php
@@ -374,4 +374,12 @@ class ExecTaskTest extends BuildFileTest
         $this->executeTarget(__FUNCTION__);
         $this->assertStringStartsWith('phploc', $this->getProject()->getProperty('envtest'));
     }
+
+    public function testEnvVars(): void
+    {
+        if ($this->windows) {
+            $this->markTestSkipped('Setting environment variables for command is not supported on windows.');
+        }
+        $this->expectPropertySet(__FUNCTION__, 'hello', 'world');
+    }
 }
