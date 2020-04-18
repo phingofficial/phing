130f4a7d3e6fe86425f54033c986c4d7917e8ee4
Siad Ardroumli <siad.ardroumli@gmail.com>
2020-04-18 15:53:27 +0200

    Update PathConvert.php

--- classes/phing/tasks/system/PathConvert.php
+++ classes/phing/tasks/system/PathConvert.php
@@ -377,8 +377,8 @@ class PathConvert extends Task
 
         // Determine the separator strings.  The dirsep and pathsep attributes
         // override the targetOS settings.
-        $dsep = FileUtils::$separator;
-        $psep = FileUtils::$pathSeparator;
+        $dsep = FileSystem::getFileSystem()->getSeparator();
+        $psep = FileSystem::getFileSystem()->getPathSeparator();
 
         if ($this->targetOS !== null) {
             $psep = $this->targetWindows ? ";" : ":";
