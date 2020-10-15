03c2766dab1009244d1da9a861998ecd5972cbeb
Jim DeLois <jimdelois@users.noreply.github.com>
2020-10-15 08:59:07 -0400

    Fix Support for Nested Exec Env Tag - Fixes #1395 (#1396)

--- classes/phing/tasks/system/ExecTask.php
+++ classes/phing/tasks/system/ExecTask.php
@@ -271,7 +271,7 @@ class ExecTask extends Task
                     continue;
                 }
                 $this->log('Setting environment variable: ' . $variable, Project::MSG_VERBOSE);
-                $envString .= $variable . '; ';
+                $envString .= 'export ' . $variable . '; ';
             }
         }
 
