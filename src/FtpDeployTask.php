5a1e9a888b59b58f802bee610d32d3c19e147ad9
Michiel Rook <michiel@michielrook.nl>
2021-01-23 21:35:13 +0100

    Use namespaced Task class

--- src/FtpDeployTask.php
+++ src/FtpDeployTask.php
@@ -19,6 +19,8 @@
 
 namespace Phing\Tasks\Ext;
 
+use Phing\Task;
+
 /**
  * FtpDeployTask
  *
@@ -42,7 +44,7 @@ namespace Phing\Tasks\Ext;
  * @since       2.3.1
  * @package     phing.tasks.ext
  */
-class FtpDeployTask extends \Task
+class FtpDeployTask extends Task
 {
     use \FileSetAware;
     use \LogLevelAware;
