0e348dd28df9b95287a258b4461aa1e190ca26ab
siad007 <siad.ardroumli@gmail.com>
2021-01-24 17:32:35 +0100

    Fixed test NS

--- tests/etc/tasks/system/TestCondition.php
+++ tests/etc/tasks/system/TestCondition.php
@@ -1,14 +1,16 @@
 <?php
 
+use Phing\Tasks\System\Condition\Condition;
+
 class TestCondition implements Condition
 {
     private $foo = null;
-    
+
     public function setFoo($value)
     {
         $this->foo = $value;
     }
-    
+
     public function evaluate()
     {
         return ($this->foo == "bar");
