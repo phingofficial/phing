df0a8b0e8b26290d257587002ad8bfb017a54755
siad007 <siad.ardroumli@gmail.com>
2021-01-24 16:19:14 +0100

    loglevelaware fixes

--- classes/phing/tasks/ext/PhpCSTask.php
+++ classes/phing/tasks/ext/PhpCSTask.php
@@ -20,6 +20,7 @@
 use Phing\Exception\BuildException;
 use Phing\Io\File;
 use Phing\Task;
+use Phing\Tasks\System\Element\LogLevelAware;
 use Phing\Type\Commandline;
 use Phing\Type\Element\FileSetAware;
 
--- classes/phing/tasks/ext/PhpLintTask.php
+++ classes/phing/tasks/ext/PhpLintTask.php
@@ -22,6 +22,7 @@ use Phing\Io\FileWriter;
 use Phing\Io\File;
 use Phing\Project;
 use Phing\Task;
+use Phing\Tasks\System\Element\LogLevelAware;
 use Phing\Type\Element\FileSetAware;
 use Phing\Util\DataStore;
 
