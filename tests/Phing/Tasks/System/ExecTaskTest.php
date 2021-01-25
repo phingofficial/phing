12d05924a248b24ccb84a846e50dd51d050c44cd
Michiel Rook <mrook@php.net>
2021-01-25 15:20:38 +0100

    Fix test execution on OSX

--- tests/Phing/Tasks/System/ExecTaskTest.php
+++ tests/Phing/Tasks/System/ExecTaskTest.php
@@ -264,7 +264,7 @@ class ExecTaskTest extends BuildFileTest
     }
 
     /**
-     * @requires OS ^(?:(?!Win).)*$
+     * @requires OS Linux|Darwin
      */
     public function testChangeToDir()
     {
