6249164d2b37f4f669c38d38408663917e2a5d5f
Mahmoud Al-Husseiny <mahmoudalhusseiny@gmail.com>
2021-02-10 03:42:20 +0200

    fix: Fix PhpEvalTask bugs with functions taking scalar and multidimensional array params

---
+++ tests/Phing/Task/System/PhpEvalTaskTest.php
@@ -0,0 +1,88 @@
+<?php
+declare(strict_types=1);
+
+namespace Phing\Task\System;
+
+use Phing\Support\BuildFileTest;
+
+/**
+ * @author Mahmoud Al-Husseiny <mahmoud@alhusseiny.io>
+ */
+class PhpEvalTaskTest extends BuildFileTest
+{
+    public static function recursiveProcess(array $arr): string
+    {
+        // ensure n-d array (n > 1)
+        $multiDimArray = false;
+        foreach ($arr as $value) {
+            if (is_array($value) && !empty($value)) {
+                $multiDimArray = true;
+                break;
+            }
+        }
+        static::assertTrue(
+            $multiDimArray,
+            'You must provide a multidimensional array for this test'
+        );
+
+        $arraySum = '';
+        array_walk_recursive($arr, function ($item) use (&$arraySum) {
+            $arraySum = $arraySum . $item;
+        });
+
+        return $arraySum;
+    }
+
+    protected function setUp(): void
+    {
+        $this->configureProject(PHING_TEST_BASE . '/etc/tasks/system/PhpEvalTest.xml');
+    }
+
+    public function testZeroParams()
+    {
+        $expected = get_include_path();
+
+        $this->executeTarget(__FUNCTION__);
+        $this->assertPropertyEquals('result', $expected);
+    }
+
+    public function testOneScalarParam()
+    {
+        $expected = trim('   test   ');
+
+        $this->executeTarget(__FUNCTION__);
+        $this->assertPropertyEquals('result', $expected);
+    }
+
+    public function testMultiScalarParams()
+    {
+        $expected = trim('##**test**##', '#');
+
+        $this->executeTarget(__FUNCTION__);
+        $this->assertPropertyEquals('result', $expected);
+    }
+
+    public function testArrayParam()
+    {
+        $expected = implode(['Phing', ' ', '3']);
+
+        $this->executeTarget(__FUNCTION__);
+        $this->assertPropertyEquals('result', $expected);
+    }
+
+    public function testMixedParams()
+    {
+        $expected = implode(' ', ['Phing', '3']);
+
+        $this->executeTarget(__FUNCTION__);
+        $this->assertPropertyEquals('result', $expected);
+    }
+
+    public function testStaticMethodWithMultiDimArrayParam()
+    {
+        $expected = static::recursiveProcess(['a', ['b', 'c'], 'd']);
+
+        $this->executeTarget(__FUNCTION__);
+        $this->assertPropertyEquals('result', $expected);
+    }
+}
