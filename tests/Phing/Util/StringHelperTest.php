d1e176ec0318be2ead9c055a29a2f6c8d6aaaed5
jawira <dev@tugal.be>
2021-05-16 19:58:28 +0200

    Add tests for SlotVar

--- tests/Phing/Util/StringHelperTest.php
+++ tests/Phing/Util/StringHelperTest.php
@@ -306,6 +306,7 @@ class StringHelperTest extends TestCase
             ['%{}', ''],
             ['%{    }', ''],
             ['  %{  slot.var  }  ', 'slot.var'],
+            ['FooBarBaz', 'FooBarBaz'],
         ];
     }
 }
